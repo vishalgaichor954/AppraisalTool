a086b64d6402e92bcc9645dd39d97a2f5a6d884f
Abhishek Singh <ssabhishek070@gmail.com>
2022-10-17 18:38:17 +0530

    worked on dasboard ui

---
+++ AppraisalTool/src/UI/AppraisalTool.Application/Controllers/DashboardController.cs
@@ -0,0 +1,29 @@
+﻿using AppraisalTool.App.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppraisalTool.App.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Dashboard()
+        {
+            JobProfilesViewmodel jobProfiles = new JobProfilesViewmodel();
+            //{
+            //       PrimaryRole="Savings Account Officer",
+            //       SecondaryRole="Current Account Officer",
+            //    Roles = { "Job Profile1","Job Profile2","Job Profile3","job profile4"}
+            //};
+            jobProfiles.PrimaryRole = "Savings Account Officer";
+            jobProfiles.SecondaryRole = "Current Account Officer";
+            jobProfiles.Roles = new List<string>() { "Job Profile1", "Job Profile2", "Job Profile3", "job profile4" };
+
+
+
+            return View(jobProfiles);
+        }
+    }
+}
