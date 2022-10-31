a1f8dcb5320708179a91a7e2ec0e4cdf9d80f804
abhishek208 <ssabhishek070@gmail.com>
2022-10-31 15:49:38 +0530

    added dynamic job roles

--- AppraisalTool/src/UI/AppraisalTool.Application/Controllers/DashboardController.cs
+++ AppraisalTool/src/UI/AppraisalTool.Application/Controllers/DashboardController.cs
@@ -15,55 +15,70 @@ namespace AppraisalTool.App.Controllers
             return View();
         }
 
-        //@Author : Abhishek Singh
+        //@Author : Abhishek Singh-- lOADING Job Profiles and Dashboard Menu Dynamically.
         public IActionResult Dashboard()
         {
             string x = HttpContext.Session.GetString("user");
             if (x == null)
             {
-                return RedirectToAction("Login","Login");
+                return RedirectToAction("Login", "Login");
             }
-            //HttpContext.Session.SetString("Role", "User");
-            //HttpContext.Session.SetString("LoggedIn", "True");
+
             var user = SessionHelper.GetObjectFromJson<LoginResponseDto>(HttpContext.Session, "user");
-            if(user != null)
+            if (user != null)
             {
                 ViewBag.FullName = user.Name;
                 ViewBag.UserRole = user.Role;
 
             }
-        //https://localhost:5000/api/Auth/getAllCards?id=1&api-version=1
+
+            //Api call to get dashboard menus
+            //https://localhost:5000/api/Auth/getAllCards?id=1&api-version=1
             HttpClient client = new HttpClient();
             client.BaseAddress = baseAddress;
-            HttpResponseMessage response = client.GetAsync(client.BaseAddress + $"/Auth/getAllCards?id={user.RoleId}&api-version=1").Result;
-            if (response.IsSuccessStatusCode)
-            {
-                var responseData = response.Content.ReadAsStringAsync().Result;
-                var res = JsonConvert.DeserializeObject<ForgetPasswordResponse>(responseData);
-                dynamic json = JsonConvert.DeserializeObject(res.Data);
-                //var menuList = JsonConvert.DeserializeObject<List<MenuListViewModel>>(json);
-                ViewBag.GetMenuCards = json;
-
-                
-                //if (res.Succeeded == true)
-                //{
-                //    return Json(true);
-                //}
-                //else
-                //{
-                //    return Json(false);
-                //}
+            //HttpResponseMessage response = client.GetAsync(client.BaseAddress + $"/Auth/getAllCards?id={user.RoleId}&api-version=1").Result;
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    var responseData = response.Content.ReadAsStringAsync().Result;
+            //    var res = JsonConvert.DeserializeObject<ForgetPasswordResponse>(responseData);
+            //    dynamic json = JsonConvert.DeserializeObject(res.Data);
                
+            //}
+        //https://localhost:5000/api/User/GetAllCard?id=3&api-version=1
+            HttpResponseMessage cardResponse = client.GetAsync(client.BaseAddress + $"/User/GetAllCard?id={user.RoleId}&api-version=1").Result;
+            if (cardResponse.IsSuccessStatusCode)
+            {
+                var responseData = cardResponse.Content.ReadAsStringAsync().Result;
+                var res = JsonConvert.DeserializeObject<ForgetPasswordResponse>(responseData);
+                dynamic json = JsonConvert.DeserializeObject(responseData);
+
+                var datacard = json.data[0].menuList;
+                Console.WriteLine(datacard);
+                ViewBag.GetMenuCards = json.data;
 
 
             }
 
+
+            //Api call to get dashboard 
+            HttpResponseMessage responses = client.GetAsync(client.BaseAddress + $"/User/GetUserJobProfile?id={user.UserId}&api-version=1").Result;
             JobProfilesViewmodel jobProfiles = new JobProfilesViewmodel();
-            jobProfiles.PrimaryRole = "Savings Account Officer";
-            jobProfiles.SecondaryRole = "Current Account Officer";
-            jobProfiles.Roles = new List<string>() { "Job Profile1", "Job Profile2", "Job Profile3", "job profile4" };
+            if (responses.IsSuccessStatusCode)
+            {
+                var responseData = responses.Content.ReadAsStringAsync().Result;
 
+                var ress = JsonConvert.DeserializeObject<ForgetPasswordResponse>(responseData);
+                jobProfiles.PrimaryRole = ress.Data.primaryRole;
+                jobProfiles.SecondaryRole = ress.Data.secondaryRole;
+                var jobProfileList = new List<string>();
 
+                foreach (string item in ress.Data.roles)
+                {
+                    jobProfileList.Add(item);
+                }
+
+                jobProfiles.Roles = jobProfileList;
+            }
 
             return View(jobProfiles);
         }
