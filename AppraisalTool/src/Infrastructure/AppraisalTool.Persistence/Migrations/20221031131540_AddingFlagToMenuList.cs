e369a8295b0a102a1eeaeb0e41091ea548cb8f2e
abhishek208 <ssabhishek070@gmail.com>
2022-11-01 12:32:35 +0530

    dyanmic sidebar

---
+++ AppraisalTool/src/Infrastructure/AppraisalTool.Persistence/Migrations/20221031131540_AddingFlagToMenuList.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AppraisalTool.Persistence.Migrations
+{
+    public partial class AddingFlagToMenuList : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MenuFlag",
+                table: "MenuLists",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Branch",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AddedOn",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(1853));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2023, 8, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(761));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2023, 7, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(514));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2023, 2, 28, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(580));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2023, 6, 30, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(822));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2023, 2, 28, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(548));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2023, 4, 30, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(456));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(974));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(946));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(866));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(917));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(1169));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(1013));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 31, 13, 15, 38, 859, DateTimeKind.Utc).AddTicks(1127));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MenuFlag",
+                table: "MenuLists");
+
+            migrationBuilder.UpdateData(
+                table: "Branch",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AddedOn",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(2336));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("1babd057-e980-4cb3-9cd2-7fdd9e525668"),
+                column: "Date",
+                value: new DateTime(2023, 8, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1716));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("3448d5a4-0f72-4dd7-bf15-c14a46b26c00"),
+                column: "Date",
+                value: new DateTime(2023, 7, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1656));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("62787623-4c52-43fe-b0c9-b7044fb5929b"),
+                column: "Date",
+                value: new DateTime(2023, 2, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1691));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("adc42c09-08c1-4d2c-9f96-2d15bb1af299"),
+                column: "Date",
+                value: new DateTime(2023, 6, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1807));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("b419a7ca-3321-4f38-be8e-4d7b6a529319"),
+                column: "Date",
+                value: new DateTime(2023, 2, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1674));
+
+            migrationBuilder.UpdateData(
+                table: "Events",
+                keyColumn: "EventId",
+                keyValue: new Guid("ee272f8b-6096-4cb6-8625-bb4bb2d89e8b"),
+                column: "Date",
+                value: new DateTime(2023, 4, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1620));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("3dcb3ea0-80b1-4781-b5c0-4d85c41e55a6"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1887));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("771cca4b-066c-4ac7-b3df-4d12837fe7e0"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1873));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("7e94bc5b-71a5-4c8c-bc3b-71bb7976237e"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1834));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("86d3a045-b42d-4854-8150-d6a374948b6e"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1856));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("ba0eb0ef-b69b-46fd-b8e2-41b4178ae7cb"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1934));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("e6a2679c-79a3-4ef1-a478-6f4c91b405b6"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1902));
+
+            migrationBuilder.UpdateData(
+                table: "Orders",
+                keyColumn: "OrderId",
+                keyValue: new Guid("f5a6a3a0-4227-4973-abb5-a63fbe725923"),
+                column: "OrderPlaced",
+                value: new DateTime(2022, 10, 27, 11, 27, 46, 355, DateTimeKind.Utc).AddTicks(1918));
+        }
+    }
+}
